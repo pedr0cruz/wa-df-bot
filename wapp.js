3e1331d4885b849dd936b05dba6d11bda4931bb2
Pedro Cruz <pcruz.qban@gmail.com>
2021-09-14 02:59:05 -0500

    .create

--- wapp.js
+++ wapp.js
@@ -28,8 +28,10 @@ var waStatus;
 
 function startBot(){ 
   venom
-    .create()
+//    .create()
 //    .create('session', (base64Qrimg, asciiQR, attempts) => {}, (statusSession, session) => {}, {disableWelcome: true, disableSpins: true, browserArgs: chromiumArgs })
+    .create( 'session', (base64Qrimg, asciiQR, attempts) => {}, (statusSession, session) => {}, {disableWelcome: true, disableSpins: true, useChrome: false, browserArgs: ['--no-sandbox'] } )  
+//  .create('session', (base64Qrimg, asciiQR, attempts) => {}, (statusSession, session) => {}, {disableWelcome: true, disableSpins: true, browserArgs: chromiumArgs })
     .then((client) => start(client))
     .catch((erro) => {
       console.log(erro);
